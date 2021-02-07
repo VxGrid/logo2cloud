100c2fc848eb3e2f66bdccf79bede5065b4f4d5b
VxGrid <mylx_66@web.de>
2021-02-07 22:13:37 +0100

    Removed some unused stuff

--- src/exporter/exporterLAS.h
+++ src/exporter/exporterLAS.h
@@ -16,17 +16,6 @@ public:
   void run() override;
 
 private:
-  /// our output file stream
-  std::ofstream out_;
-
-  laszip_POINTER laszPtr_{};
-
   laszip_POINTER laszip_writer_{};
-
   laszip_header *header_{};
-  /// header of las file
-  //std::unique_ptr<liblas::Header> lasHeader_;
-
-  /// the actual las writer
-  //std::unique_ptr<liblas::Writer> lasWriter_;
 };
